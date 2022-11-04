7f06322dc9201362f7f2fb76ca732038be69fc95
Abdallah Ehab <abdallah.alkashawy@gmail.com>
2022-11-04 18:39:32 +0100

    Done

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const mongoose = require('mongoose');
+
+const MongoURI = 
+'mongodb+srv://Pedri:pedri@cluster0.7oxvhwk.mongodb.net/?retryWrites=true&w=majority' ;
+
