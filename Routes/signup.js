3c1414e50d226f1d30ef55de0a7e753a92775336
Abdallah Ehab <abdallah.alkashawy@gmail.com>
2022-11-06 15:37:34 +0100

    z

--- Routes/signup.js
+++ Routes/signup.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const selectcountry = require('./middleware/selectcountry');
-const Administrator = require("../Model/Administrator");
-const CorporateTrainee = require("../Model/CorporateTrainee");
-const Instructor = require("../Model/Instructor");
 //convert string to int we use "parseInt()"
 
 // to equate two types we use "==="
@@ -13,76 +10,56 @@ const Instructor = require("../Model/Instructor");
         res.json({msg: `Update contact ${req.params.id}`}); `});
 */
 
-//hellooo
-
 router.post("/addingAdmin",async(req, res)=>{
-    const biho= await Administrator.findOne({UserName: req.body.Name})
-    if(biho==null){
     const addAdmin=  await Administrator.create(
         { UserName: req.body.Name,
         Country: req.body.Country,
         Password: req.body.Password,
         Type:"Admin"
-        
     });
 
      
-    res.status(200).send(addAdmin);}
-
-    else{
-        res.send("pick another username please");
-
-    }
+    res.status(200).send(addAdmin);
 });
 
 
-
+//jjj
 router.post("/addingCorporateTrainee",async(req, res)=>{
-    const biho= await CorporateTrainee.findOne({UserName: req.body.Name})
-    if(biho==null){
     const addTrainee=  await CorporateTrainee.create(
         { UserName: req.body.Name,
         Country: req.body.Country,
         Password: req.body.Password,
         Type:"CorporateTrainee"
-        
+                   
     });
 
      
-    res.status(200).send(addTrainee);}
-
-    else{
-        res.send("pick another username please");
-
-    }
+    res.status(200).send(addTrainee);
 });
 
 
 
 router.post("/addingInstructor",async(req, res)=>{
-    const biho= await Instructor.findOne({UserName: req.body.Name})
-    if(biho==null){
     const addInstructor=  await Instructor.create(
         { UserName: req.body.Name,
         Country: req.body.Country,
         Password: req.body.Password,
         Type:"Instructor"
-        
     });
 
      
-    res.status(200).send(addInstructor);}
-
-    else{
-        res.send("pick another username please");
-
-    }
+    res.status(200).send(addInstructor);
 });
+
+
 
 
 
 router.post('/guest/signup', (req, res)=>{
     const country = selectcountry(req.body.country);
     res.send(country);
+    const newIndividualTrainee = new IndividualTrainee({
+    
+    }); 
 })
 module.exports = router;
