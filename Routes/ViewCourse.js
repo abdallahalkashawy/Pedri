e275bebf4e380688bbb30d006c1d2c84b44ef4d3
Abdallah Ehab <abdallah.alkashawy@gmail.com>
2022-11-17 12:31:58 +0100

    Hussein Effort

--- Routes/ViewCourse.js
+++ Routes/ViewCourse.js
@@ -88,6 +88,53 @@ router.get('/viewallInstructors', (req,res) => {
     // }
 });
 
+/// HUSSEIN
+router.get('/fiterrate', async (req,res) => {
+  try{
+    if(req.body.rate && req.body.subject== null){
+    var response = await course.aggregate([{ $match: { rate:req.body.rate}}]);
+    res.send(response);
+    }else if(req.body.subject && req.body.rate == null ){
+    var response22 =await course.aggregate([{ $match: { subject:req.body.subject}}]);
+    res.send(response22);
+    }else if(req.body.subject && req.body.rate){
+    var response1000 = await course.aggregate([{ $match: { subject:req.body.subject,rate:req.body.rate}}]);
+    res.send(response1000);
+    }
+
+  }catch(error){
+    res.status(500).send(error);
+  }
+  });
+
+  router.get('/filterpriceee', async (req,res) => {
+    try{
+      var response99 = await course.find({price:{$lte:req.body.price}});      
+      res.send(response99);    
+    }catch(error){
+      res.status(500).send(error);
+    }
+    });
+
+    router.get('/search', async (req,res) => {
+      try{
+        if(req.body.subject){
+          var response = await course.aggregate([{ $match: { subject:req.body.subject}}]);
+          res.send(response);
+        }else if(req.body.instructor){
+          var repo = await course.aggregate([{$match: {instructor:req.body.instructor}}]);
+          res.send(repo);
+        }else if(req.body.title){
+          var repoo = await course.aggregate([{$match: {title:req.body.title}}])
+          res.send(repoo);
+        }
+      }catch(error){
+        res.status(500).send(error);
+      }
+});
+
+/// HUSSIEN
+
 router.get('/InstructorFilterCourse/:inid', async (req,res) => {
     course.find({instructor :req.params.inid , subject : req.body.subject}).select('title subject instructor-_id').exec((err, course) => {
         if(err) {
