1b3fcf8829b343629a055b1824d6e9226bf2b9b7
mohamedbahaa001 <mohamedbhaa02@gmail.com>
2022-12-10 16:53:23 +0100

    Added email reset pass and change pass

---
+++ Routes/passwordReset.js
@@ -0,0 +1,64 @@
+//new
+const { Administrator } = require("../Model/Administrator");
+const Token = require("../Model/token");
+const sendEmail = require("../utils/sendEmail");
+const crypto = require("crypto");
+const Joi = require("joi");
+const express = require("express");
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+    try {
+        const schema = Joi.object({ email: Joi.string().email().required() });
+        const { error } = schema.validate(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
+
+        const user = await Administrator.findOne({ email: req.body.email });
+        if (!user)
+            return res.status(400).send("user with given email doesn't exist");
+
+        let token = await Token.findOne({ userId: user._id });
+        if (!token) {
+            token = await new Token({
+                userId: user._id,
+                token: crypto.randomBytes(32).toString("hex"),
+            }).save();
+        }
+
+        const link = `${process.env.BASE_URL}/password-reset/${user._id}/${token.token}`;
+        await sendEmail(user.email, "Password reset", link);
+
+        res.send("password reset link sent to your email account");
+    } catch (error) {
+        res.send("An error occured");
+        console.log(error);
+    }
+});
+
+router.post("/:userId/:token", async (req, res) => {
+    try {
+        const schema = Joi.object({ Password: Joi.string().required() });
+        const { error } = schema.validate(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
+
+        const user = await Administrator.findById(req.params.userId);
+        if (!user) return res.status(400).send("invalid link or expired");
+
+        const token = await Token.findOne({
+            userId: user._id,
+            token: req.params.token,
+        }); 
+        if (!token) return res.status(400).send("Invalid link or expired");
+
+        user.Password = req.body.Password;
+        await user.save();
+        await token.delete();
+
+        res.send("password reset sucessfully.");
+    } catch (error) {
+        res.send("An error occured");
+        console.log(error);
+    }
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+require("dotenv").config(); //new
 const express = require("express");
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
-const routes = require('./Routes/signup');
+const passwordReset = require("./Routes/passwordReset"); //new
+const routes = require('./Routes/signup'); 
+const users = require('./Routes/users');  //new
 const courses = require('./Routes/ViewCourse');
 const app = express();
 const port = 3000;
@@ -21,4 +24,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(routes);
 app.use(courses);
+app.use("/api/users", users); //new
+app.use("/api/password-reset", passwordReset); //new
+///app.use("/password-reset", passwordReset);
 // app.use(errorHandler)
+//test again
