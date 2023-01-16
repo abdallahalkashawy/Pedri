c5cdb84f4b5ca0bd597386af048c205035dfd9f1
Abdallah Ehab <abdallah.alkashawy@gmail.com>
2023-01-16 18:15:09 +0100

    Sign UP individual trainee and instructor

--- Model/Instructor.js
+++ Model/Instructor.js
@@ -5,23 +5,35 @@ const InstructorSchema = new Schema(
   {
     UserName: {
       type: String,
-      required: true,
+      unique: [true, "Username already exists"],
+      required: true
+    },
+    Email: {
+      type: String,
+      unique: [true, "Email already exists"],
+      required: false,
     },
     Password: {
       type: String,
       required: true,
     },
+    FirstName: {
+      type: String,
+      required: false
+    },
+    LastName: {
+      type: String,
+      required: false
+    },
+    Gender: {
+      type: String,
+      required: false
+    },
     Country: {
       type: String,
-      required: false,
-    },
-    Type: {
-      type: String,
-      required: true,
-    },
-  },
-  { timestamps: true }
-);
+      required: false
+    }
+  });
 
 const Instructor = mongoose.model("Instructor", InstructorSchema);
 module.exports = Instructor;
